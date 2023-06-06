902f83216e452adeb652513e5b5303db152a1967
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2023-06-06 15:55:08 +0200

    lets not be stupid for once

--- Source/LogSubsystem/Public/LoggingSubsystem.h
+++ Source/LogSubsystem/Public/LoggingSubsystem.h
@@ -14,8 +14,11 @@ public:
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
 
+    UFUNCTION(BlueprintCallable)
     void SetLoggingEnabled(bool bEnabled);
+    UFUNCTION(BlueprintCallable)
     bool IsLoggingEnabled() const;
+    UFUNCTION(BlueprintCallable)
     void LogToFile(const FString& LogMessage);
 
 private:
